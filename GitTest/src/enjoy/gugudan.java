fd585ee2e685910c7ae03d63c9fb843b8ec530f5
Rock hyun Choi <choimosi@gmail.com>
2015-06-19 17:58:17 +0900

    First project start

---
+++ GitTest/src/enjoy/gugudan.java
@@ -0,0 +1,8 @@
+package enjoy;
+
+public class gugudan {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
---
+++ GitTest/src/enjoy/package-info.java
@@ -0,0 +1,33 @@
+/////////////////////////////////////////////
+/// @file package-info.java
+/// @section 파일생성정보
+///    |    항  목       |      내  용       |
+///    | :-------------: | -------------   |
+///    | File name | package-info.java |    
+///    | Package | enjoy |    
+///    | Project name | GitTest |    
+///    | Type name | package-info |    
+///    | Company | DGIST | 
+///    | Create Date | 2015 Jun 19, 2015 5:38:44 PM |
+///    | Author | Owner |
+///    | File Version | v1.0 |
+///    | Other.. | ... |
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+/// @class package-info
+///enjoy \n
+///   ㄴ package-info.java
+/// @section 클래스작성정보
+///    |    항  목       |      내  용       |
+///    | :-------------: | -------------   |
+///    | Company | DGIST |    
+///    | Author | Owner |
+///    | Date | Jun 19, 2015 5:38:44 PM |
+///    | Class Version | v1.0 |
+///    | 작업자 | Owner, Others... |
+/// @section 상세설명
+/// - 이 클래스는...
+///   -# ㅇㅇ
+/////////////////////////////////////////////
+package enjoy;
+
