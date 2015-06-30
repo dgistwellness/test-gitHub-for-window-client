2e0692646228abc0634c4d74ecc932c0f013f7dd
dgistwellness <dgistwellness@gmail.com>
2015-06-30 11:12:48 +0900

    upgrade!

--- GitTest/src/enjoy/gugudan.java
+++ GitTest/src/enjoy/gugudan.java
@@ -5,7 +5,9 @@ public class gugudan {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		for(int i=0;i<10;i++){
+			//wow
 			System.out.println(i);
+			
 		}
 	}
 }
